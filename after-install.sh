b9c4436a39fa3a838a5a9514cbacc2130373de4d
Kirill Timofeev <kvt@hulu.com>
2014-04-15 14:16:01 -0700

    file upload form now configured via urepo.conf

--- after-install.sh
+++ after-install.sh
@@ -4,17 +4,23 @@ set -u
 
 configure_urepo() {
     . /etc/urepo/urepo.conf
+    data=""
     mkdir -p $RPM_REPO_ROOT && cd $RPM_REPO_ROOT
     for release in $RPM_RELEASES; do
+        options=""
         for component in $RPM_COMPONENTS; do
+            options+="${options:+, }\"$component\""
             for arch in $RPM_ARCHITECTURES; do
                 mkdir -p "$release/$component/$arch"
             done
         done
+        data+="${data:+, }$release: [$options]"
     done
     mkdir $DEB_REPO_ROOT && cd $DEB_REPO_ROOT
     for codename in $DEB_CODENAMES; do
+        options=""
         for component in $DEB_COMPONENTS; do
+            options+="${options:+, }\"$component\""
             pool_dir=pool/$codename/$component
             mkdir -p $pool_dir
             for arch in $DEB_ARCHITECTURES; do
@@ -32,12 +38,14 @@ configure_urepo() {
             -o APT::FTPArchive::Release::Architectures="$DEB_ARCHITECTURES" \
             -o APT::FTPArchive::Release::Components="$DEB_COMPONENTS" \
             release dists/$codename > dists/$codename/Release
+        data+=", $codename: [$options]"
     done
     mkdir -p $UREPO_UPLOAD_DIR
     chmod 0733 $UREPO_UPLOAD_DIR
     chown -R www-data:www-data $UREPO_UPLOAD_DIR/..
     host_name=$(hostname -f)
     sed -i -e "s/server_name[^;]*;/server_name ${host_name};/" /etc/urepo/urepo-nginx
+    sed -i -e "s/\(var data = {\).*\(};\)/\1$data\2/" $UREPO_ROOT/index.html
     ln -nsf /etc/urepo/urepo-nginx /etc/nginx/sites-enabled/urepo-nginx
     /etc/init.d/nginx restart
 }
